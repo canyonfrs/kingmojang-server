3fdb7822141dba9fe888f3a6aafedce2e6b82df5
SeoSeunghun <proto_seo@naver.com>
2023-03-04 14:25:40 +0900

    feat: add Memo domain

---
+++ src/main/kotlin/app/kingmojang/domain/memo/domain/Memo.kt
@@ -0,0 +1,30 @@
+package app.kingmojang.domain.memo.domain
+
+import app.kingmojang.domain.member.domain.Member
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+class Memo(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "memo_id")
+    val id: Long? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    val writerId: Member,
+
+    var title: String,
+    @Lob
+    var content: String,
+
+    var likeCount: Int,
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    val createdAt: LocalDateTime,
+
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: LocalDateTime,
+) {
+}

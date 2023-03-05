9c8ff92284cd54e3b60c5bdae6d651ebc8e9692a
SeoSeunghun <proto_seo@naver.com>
2023-03-05 19:48:00 +0900

    feat: validate SignupRequestDto

--- src/main/kotlin/app/kingmojang/domain/member/api/AuthController.kt
+++ src/main/kotlin/app/kingmojang/domain/member/api/AuthController.kt
@@ -3,7 +3,7 @@ package app.kingmojang.domain.member.api
 import app.kingmojang.domain.member.application.AuthService
 import app.kingmojang.domain.member.dto.request.SignupRequest
 import app.kingmojang.global.common.response.CommonResponse
-import app.kingmojang.global.common.response.ResponseStatus
+import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -18,15 +18,13 @@ class AuthController(
 ) {
 
     @PostMapping("/members")
-    fun signup(@RequestBody  request: SignupRequest): ResponseEntity<CommonResponse<Void>> {
+    fun signup(@RequestBody @Valid request: SignupRequest): ResponseEntity<CommonResponse<Void>> {
         val id = authService.signup(request)
 
         val uri = ServletUriComponentsBuilder
             .fromCurrentContextPath().path("/members/{id}")
             .buildAndExpand(id).toUri()
 
-        return ResponseEntity.created(uri).body(CommonResponse.of(ResponseStatus.SUCCESS))
+        return ResponseEntity.created(uri).body(CommonResponse.success())
     }
-
-
 }

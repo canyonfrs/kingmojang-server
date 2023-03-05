a75d493b91cc1c3dd27cc7140cca8b3167e1cf45
SeoSeunghun <proto_seo@naver.com>
2023-03-05 19:48:00 +0900

    feat: validate SignupRequestDto

--- src/main/kotlin/app/kingmojang/domain/member/dto/request/SignupRequest.kt
+++ src/main/kotlin/app/kingmojang/domain/member/dto/request/SignupRequest.kt
@@ -1,17 +1,20 @@
 package app.kingmojang.domain.member.dto.request
 
 import app.kingmojang.domain.member.domain.MemberType
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
 data class SignupRequest(
-    val username: String,
-    val password: String,
-    val nickname: String,
-    val email: String,
-    val isAuthorizedAccount: Boolean,
+    @field:NotBlank val username: String,
+    @field:NotBlank val password: String,
+    @field:NotBlank val nickname: String,
+    @field:Email val email: String,
+    @field:NotNull val isAuthorizedAccount: Boolean,
     val introduce: String?,
     val youtube: String?,
     val broadcastLink: String?,
     val donationLink: String?,
-    val memberType: MemberType,
+    @field:NotNull val memberType: MemberType,
 ) {
 }

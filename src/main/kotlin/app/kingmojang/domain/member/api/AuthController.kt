e88be8b70e670fa0f2b11dbf73c786ccbf425608
SeoSeunghun <proto_seo@naver.com>
2023-03-05 13:14:03 +0900

    feat: add AuthController
    - implement Signup api

---
+++ src/main/kotlin/app/kingmojang/domain/member/api/AuthController.kt
@@ -0,0 +1,32 @@
+package app.kingmojang.domain.member.api
+
+import app.kingmojang.domain.member.application.AuthService
+import app.kingmojang.domain.member.dto.request.SignupRequest
+import app.kingmojang.global.common.response.CommonResponse
+import app.kingmojang.global.common.response.ResponseStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+
+@RestController
+@RequestMapping("/api/v1")
+class AuthController(
+    private val authService: AuthService,
+) {
+
+    @PostMapping("/members")
+    fun signup(@RequestBody  request: SignupRequest): ResponseEntity<CommonResponse<Void>> {
+        val id = authService.signup(request)
+
+        val uri = ServletUriComponentsBuilder
+            .fromCurrentContextPath().path("/members/{id}")
+            .buildAndExpand(id).toUri()
+
+        return ResponseEntity.created(uri).body(CommonResponse.of(ResponseStatus.SUCCESS))
+    }
+
+
+}

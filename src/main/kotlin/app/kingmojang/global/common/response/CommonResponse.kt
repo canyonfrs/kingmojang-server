480044c44bbdeab12df18432dc21dcb1893d0c34
SeoSeunghun <proto_seo@naver.com>
2023-03-05 19:48:58 +0900

    feat: update CommonRespones create method

--- src/main/kotlin/app/kingmojang/global/common/response/CommonResponse.kt
+++ src/main/kotlin/app/kingmojang/global/common/response/CommonResponse.kt
@@ -1,6 +1,10 @@
 package app.kingmojang.global.common.response
 
+import app.kingmojang.global.common.response.ResponseStatus.*
+import app.kingmojang.global.exception.ErrorCodes
 import com.fasterxml.jackson.annotation.JsonInclude
+import org.springframework.http.HttpStatus
+import java.time.LocalDateTime
 
 data class CommonResponse<T>(
     val status: ResponseStatus,
@@ -9,19 +13,35 @@ data class CommonResponse<T>(
     val message: String?,
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    val data: T?
-){
-    companion object{
-        fun of(status: ResponseStatus): CommonResponse<Void> {
-            return CommonResponse(status, null, null)
+    val data: T?,
+) {
+    companion object {
+        fun success(): CommonResponse<Void> {
+            return CommonResponse(SUCCESS, null, null)
         }
 
-        fun of(status: ResponseStatus, message: String): CommonResponse<Void> {
-            return CommonResponse(status, message, null)
+        fun <T> success(data: T): CommonResponse<T> {
+            return CommonResponse(SUCCESS, null, data)
         }
 
-        fun <T> of(status: ResponseStatus, data: T): CommonResponse<T> {
-            return CommonResponse(status, null, data)
+        fun <T> fail(data: T): CommonResponse<T> {
+            return CommonResponse(FAIL, null, data)
+        }
+
+        fun error(message: String, data: ExceptionBody): CommonResponse<ExceptionBody> {
+            return CommonResponse(ERROR, message, data)
+        }
+    }
+}
+
+data class ExceptionBody(
+    val name: String,
+    val status: HttpStatus,
+    val timestamp: LocalDateTime = LocalDateTime.now(),
+) {
+    companion object {
+        fun of(code: ErrorCodes): ExceptionBody {
+            return ExceptionBody(code.name, code.status)
         }
     }
 }

db8a641ef08f8906d0167756485212043247be7f
SeoSeunghun <proto_seo@naver.com>
2023-03-06 20:11:39 +0900

    feat: add login api, refresh api

--- src/main/kotlin/app/kingmojang/domain/member/api/AuthController.kt
+++ src/main/kotlin/app/kingmojang/domain/member/api/AuthController.kt
@@ -1,7 +1,10 @@
 package app.kingmojang.domain.member.api
 
 import app.kingmojang.domain.member.application.AuthService
+import app.kingmojang.domain.member.dto.request.LoginRequest
+import app.kingmojang.domain.member.dto.request.RefreshRequest
 import app.kingmojang.domain.member.dto.request.SignupRequest
+import app.kingmojang.domain.member.dto.response.TokenResponse
 import app.kingmojang.global.common.response.CommonResponse
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
@@ -26,5 +29,17 @@ class AuthController(
             .buildAndExpand(id).toUri()
 
         return ResponseEntity.created(uri).body(CommonResponse.success())
+    }
+
+    @PostMapping("/login")
+    fun login(@RequestBody @Valid request: LoginRequest): ResponseEntity<CommonResponse<TokenResponse>> {
+        val tokenResponse = authService.login(request)
+        return ResponseEntity.ok(CommonResponse.success(tokenResponse))
+    }
+
+    @PostMapping("/refresh")
+    fun refresh(@RequestBody @Valid request: RefreshRequest): ResponseEntity<CommonResponse<TokenResponse>> {
+        val tokenResponse = authService.refresh(request)
+        return ResponseEntity.ok(CommonResponse.success(tokenResponse))
     }
 }

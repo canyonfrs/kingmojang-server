c47d68c087abb6edb73371715cb2d843fd6b8fca
SeoSeunghun <proto_seo@naver.com>
2023-03-06 20:16:00 +0900

    refactor: separate member type for nickname api

--- src/main/kotlin/app/kingmojang/domain/member/application/MemberService.kt
+++ src/main/kotlin/app/kingmojang/domain/member/application/MemberService.kt
@@ -1,5 +1,6 @@
 package app.kingmojang.domain.member.application
 
+import app.kingmojang.domain.member.domain.MemberType
 import app.kingmojang.domain.member.repository.MemberRepository
 import app.kingmojang.global.exception.common.InvalidInputException
 import org.springframework.stereotype.Service
@@ -18,11 +19,11 @@ class MemberService(
         return memberRepository.existsByUsername(username)
     }
 
-    fun existsNickname(nickname: String): Boolean {
-        if (isBlank(nickname)) {
+    fun existsNickname(nickname: String, type: String): Boolean {
+        if (isBlank(nickname) || !MemberType.isValidate(type)) {
             throw InvalidInputException(nickname)
         }
-        return memberRepository.existsByNickname(nickname)
+        return memberRepository.existsByNicknameAndType(nickname, MemberType.valueOf(type))
     }
 
     fun existsEmail(email: String): Boolean {

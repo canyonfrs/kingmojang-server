b5756b917bd400df8afb3fb8f515698e8783a257
SeoSeunghun <proto_seo@naver.com>
2023-03-06 20:23:15 +0900

    feat: add JWT Token Filter

---
+++ src/main/kotlin/app/kingmojang/global/security/JwtExceptionFilter.kt
@@ -0,0 +1,33 @@
+package app.kingmojang.global.security
+
+import app.kingmojang.global.common.response.CommonResponse
+import app.kingmojang.global.common.response.ExceptionBody
+import app.kingmojang.global.exception.CommonException
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class JwtExceptionFilter : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain,
+    ) {
+        val objectMapper = ObjectMapper()
+        try {
+            filterChain.doFilter(request, response)
+        } catch (e: CommonException) {
+            val errorCode = e.codeBook
+            response.status = errorCode.status.value()
+            response.contentType = MediaType.APPLICATION_JSON_VALUE
+            response.characterEncoding = "UTF-8"
+            objectMapper.writeValue(response.writer, CommonResponse.error(errorCode.name, ExceptionBody.of(errorCode)))
+        }
+    }
+}

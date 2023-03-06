89e1ac21eeeadc166a4aacec7c25ed8fe043bfe7
SeoSeunghun <proto_seo@naver.com>
2023-03-06 20:23:53 +0900

    feat: add Security Config

--- src/main/kotlin/app/kingmojang/global/config/SecurityConfig.kt
+++ src/main/kotlin/app/kingmojang/global/config/SecurityConfig.kt
@@ -1,5 +1,7 @@
 package app.kingmojang.global.config
 
+import app.kingmojang.global.security.JwtAuthFilter
+import app.kingmojang.global.security.JwtExceptionFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -7,23 +9,29 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtAuthFilter: JwtAuthFilter,
+    private val jwtExceptionFilter: JwtExceptionFilter,
+) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http.formLogin().disable()
-
         http {
             cors { disable() }
             csrf { disable() }
             httpBasic { disable() }
             sessionManagement { SessionCreationPolicy.STATELESS }
-            authorizeHttpRequests {
-                authorize("/api/v1/**", permitAll)
-            }
         }
+        http.authorizeHttpRequests {
+            it.requestMatchers("/api/v1/**").permitAll()
+                .anyRequest().authenticated()
+        }
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
+        http.addFilterBefore(jwtExceptionFilter, JwtAuthFilter::class.java)
 
         return http.build()
     }

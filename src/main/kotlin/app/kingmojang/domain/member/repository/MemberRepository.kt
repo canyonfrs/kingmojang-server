a8c3d77cf227d3127dfffd58234e20085306ec0d
SeoSeunghun <proto_seo@naver.com>
2023-03-06 20:19:57 +0900

    feat: add findByUser and refactor existsByNickname method

--- src/main/kotlin/app/kingmojang/domain/member/repository/MemberRepository.kt
+++ src/main/kotlin/app/kingmojang/domain/member/repository/MemberRepository.kt
@@ -1,10 +1,12 @@
 package app.kingmojang.domain.member.repository
 
 import app.kingmojang.domain.member.domain.Member
+import app.kingmojang.domain.member.domain.MemberType
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MemberRepository : JpaRepository<Member, Long> {
+    fun findByUsername(username: String): Member?
     fun existsByUsername(username: String): Boolean
-    fun existsByNickname(nickname: String): Boolean
+    fun existsByNicknameAndType(nickname: String, type: MemberType): Boolean
     fun existsByEmail(email: String): Boolean
 }

340c7e745f0fd5d75271ac65d3c2aff36c35a3dd
SeoSeunghun <proto_seo@naver.com>
2023-03-15 20:17:01 +0900

    feat: add comment logic

--- src/main/kotlin/app/kingmojang/domain/comment/domain/Comment.kt
+++ src/main/kotlin/app/kingmojang/domain/comment/domain/Comment.kt
@@ -1,5 +1,6 @@
 package app.kingmojang.domain.comment.domain
 
+import app.kingmojang.domain.comment.dto.request.CommentRequest
 import app.kingmojang.domain.member.domain.Member
 import app.kingmojang.domain.memo.domain.Memo
 import jakarta.persistence.*
@@ -14,24 +15,18 @@ class Comment(
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
-    val writerId: Member,
+    val writer: Member,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "memo_id")
-    val memoId: Memo,
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_id")
-    val parent : Comment?,
-
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "parent", orphanRemoval = true)
-    @Column(name = "child_comments")
-    val childComments: MutableList<Comment> = mutableListOf(),
+    val memo: Memo,
 
     @Lob
     var content: String,
 
     var likeCount: Int,
+
+    var replyCount: Int,
 
     @Column(name = "created_at", nullable = false, updatable = false)
     val createdAt: LocalDateTime,
@@ -39,4 +34,37 @@ class Comment(
     @Column(name = "updated_at", nullable = false)
     var updatedAt: LocalDateTime,
 ) {
+    companion object {
+        fun create(writer: Member, memo: Memo, request: CommentRequest): Comment {
+            val now = LocalDateTime.now()
+            memo.increaseCommentCount()
+            return Comment(
+                writer = writer,
+                memo = memo,
+                content = request.content,
+                likeCount = 0,
+                replyCount = 0,
+                createdAt = now,
+                updatedAt = now
+            )
+        }
+    }
+
+    fun update(request: CommentRequest): Comment {
+        this.content = request.content
+        this.updatedAt = LocalDateTime.now()
+        return this
+    }
+
+    fun increaseReplyCount() {
+        this.replyCount++
+    }
+
+    fun decreaseReplyCount() {
+        this.replyCount--
+    }
+
+    fun remove() {
+        this.memo.decreaseCommentCount()
+    }
 }

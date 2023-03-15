a9a4e5c542383ca5a3d69ccae87d0fdfde101779
SeoSeunghun <proto_seo@naver.com>
2023-03-15 20:16:27 +0900

    feat: add NotFound Comment, Reply error code

--- src/main/kotlin/app/kingmojang/global/exception/ErrorCodes.kt
+++ src/main/kotlin/app/kingmojang/global/exception/ErrorCodes.kt
@@ -9,10 +9,12 @@ enum class ErrorCodes(val status: HttpStatus, val description: String) {
     INVALID_INPUT(HttpStatus.BAD_REQUEST, ""),
     NOT_FOUND_USERNAME(HttpStatus.NOT_FOUND, ""),
     NOT_FOUND_MEMO(HttpStatus.NOT_FOUND, ""),
+    NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, ""),
+    NOT_FOUND_REPLY(HttpStatus.NOT_FOUND, ""),
     UNHANDLED_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, ""),
     INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
     INVALID_JWT_SIGNATURE(HttpStatus.UNAUTHORIZED, "토큰의 시그니처가 잘못되었습니다."),
     UNSUPPORTED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "지원하지 않는 JWT 토큰 입니다."),
     EXPIRED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "Access 토큰이 만료되었습니다."),
-    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "Refresh 토큰이 만료되었습니다.")
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "Refresh 토큰이 만료되었습니다."),
 }

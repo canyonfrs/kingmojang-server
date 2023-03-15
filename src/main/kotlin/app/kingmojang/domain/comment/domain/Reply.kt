7d26abcf8e2cb64536e31094ca791694db906d6d
SeoSeunghun <proto_seo@naver.com>
2023-03-15 20:17:13 +0900

    feat: add reply domain

---
+++ src/main/kotlin/app/kingmojang/domain/comment/domain/Reply.kt
@@ -0,0 +1,66 @@
+package app.kingmojang.domain.comment.domain
+
+import app.kingmojang.domain.comment.dto.request.ReplyRequest
+import app.kingmojang.domain.member.domain.Member
+import app.kingmojang.domain.memo.domain.Memo
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+class Reply(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reply_id")
+    val id: Long? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    val writer: Member,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    val comment: Comment,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memo_id")
+    val memo: Memo,
+
+    @Lob
+    var content: String,
+
+    var likeCount: Int,
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    val createdAt: LocalDateTime,
+
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: LocalDateTime,
+) {
+    companion object {
+        fun create(writer: Member, memo: Memo, comment: Comment, request: ReplyRequest): Reply {
+            val now = LocalDateTime.now()
+            memo.increaseCommentCount()
+            comment.increaseReplyCount()
+            return Reply(
+                writer = writer,
+                comment = comment,
+                memo = memo,
+                content = request.content,
+                likeCount = 0,
+                createdAt = now,
+                updatedAt = now,
+            )
+        }
+    }
+
+    fun update(request: ReplyRequest): Reply {
+        this.content = request.content
+        this.updatedAt = LocalDateTime.now()
+        return this
+    }
+
+    fun remove() {
+        this.comment.decreaseReplyCount()
+        this.memo.decreaseCommentCount()
+    }
+}

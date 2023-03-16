ffb95be55f797c8ab2eeb8ff82abe44c93ae1352
SeoSeunghun <proto_seo@naver.com>
2023-03-16 22:51:15 +0900

    feat: check if memos has been liked

--- src/main/kotlin/app/kingmojang/domain/memo/repository/MemoQueryRepository.kt
+++ src/main/kotlin/app/kingmojang/domain/memo/repository/MemoQueryRepository.kt
@@ -1,11 +1,15 @@
 package app.kingmojang.domain.memo.repository
 
+import app.kingmojang.domain.like.domain.QMemoLike.*
 import app.kingmojang.domain.member.domain.QMember.member
 import app.kingmojang.domain.memo.domain.QMemo.*
 import app.kingmojang.domain.memo.dto.response.MemoDto
 import app.kingmojang.domain.memo.dto.response.QMemoDto
 import app.kingmojang.global.common.request.NoOffsetPageRequest
 import com.querydsl.core.BooleanBuilder
+import com.querydsl.core.types.dsl.BooleanExpression
+import com.querydsl.core.types.dsl.Expressions
+import com.querydsl.jpa.JPAExpressions
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
 
@@ -19,7 +23,7 @@ class MemoQueryRepository(
         builder.and(memo.updatedAt.lt(request.updateAt))
 
         return queryFactory
-            .select(createQMemoDto())
+            .select(createQMemoDto(Expressions.FALSE))
             .from(memo)
             .join(memo.writer, member)
             .where(
@@ -32,7 +36,7 @@ class MemoQueryRepository(
 
     fun findMemosInUpdatedOrder(size: Int): List<MemoDto> {
         return queryFactory
-            .select(createQMemoDto())
+            .select(createQMemoDto(Expressions.FALSE))
             .from(memo)
             .join(memo.writer, member)
             .orderBy(memo.updatedAt.desc())
@@ -42,20 +46,39 @@ class MemoQueryRepository(
 
     fun findByIdOrNull(id: Long): MemoDto? {
         return queryFactory
-            .select(createQMemoDto())
+            .select(createQMemoDto(Expressions.FALSE))
             .from(memo)
             .join(memo.writer, member)
             .where(memo.id.eq(id))
             .fetchOne()
     }
 
-    private fun createQMemoDto() = QMemoDto(
+    fun findByIdOrNullWithUsername(id: Long, username: String): MemoDto? {
+        return queryFactory
+            .select(createQMemoDto(existsMemoLike(id, username)))
+            .from(memo)
+            .join(memo.writer, member)
+            .where(memo.id.eq(id))
+            .fetchOne()
+    }
+
+    private fun existsMemoLike(memoId: Long, username: String): BooleanExpression =
+        if (JPAExpressions
+                .selectFrom(memoLike)
+                .where(memoLike.memo.id.eq(memoId).and(memoLike.member.username.eq(username)))
+                .fetchOne() == null
+        ) Expressions.FALSE
+        else Expressions.TRUE
+
+
+    private fun createQMemoDto(isLike:BooleanExpression) = QMemoDto(
         memo.id,
         memo.title,
         memo.writer.nickname,
         memo.content,
         memo.likeCount,
         memo.commentCount,
+        isLike,
         memo.createdAt,
         memo.updatedAt,
         memo.font.name,

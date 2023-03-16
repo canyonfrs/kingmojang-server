6d7e95a1073e0f343b824de96011deb1e4098755
SeoSeunghun <proto_seo@naver.com>
2023-03-16 22:45:50 +0900

    feat: add memo like feature

--- src/main/kotlin/app/kingmojang/domain/memo/application/MemoService.kt
+++ src/main/kotlin/app/kingmojang/domain/memo/application/MemoService.kt
@@ -2,6 +2,9 @@ package app.kingmojang.domain.memo.application
 
 import app.kingmojang.domain.comment.dto.response.CommentsResponse
 import app.kingmojang.domain.comment.repository.CommentQueryRepository
+import app.kingmojang.domain.like.domain.MemoLike
+import app.kingmojang.domain.like.exception.NotFoundMemoLikeException
+import app.kingmojang.domain.like.repository.MemoLikeRepository
 import app.kingmojang.domain.member.exception.NotFoundUsernameException
 import app.kingmojang.domain.member.repository.MemberRepository
 import app.kingmojang.domain.memo.domain.Memo
@@ -26,6 +29,7 @@ class MemoService(
     private val memoQueryRepository: MemoQueryRepository,
     private val commentQueryRepository: CommentQueryRepository,
     private val memberRepository: MemberRepository,
+    private val memoLikeRepository: MemoLikeRepository,
 ) {
 
     @Transactional
@@ -43,17 +47,30 @@ class MemoService(
         return memo.update(request).id!!
     }
 
-    private fun validate(tokenUsername: String, reqUsername: String) {
-        if (tokenUsername != reqUsername) {
-            throw CommonException(INVALID_JWT_TOKEN)
-        }
-    }
-
     @Transactional(readOnly = true)
     fun readMemo(id: Long, request: CommonPageRequest): MemoResponse {
         val memo = memoQueryRepository.findByIdOrNull(id) ?: throw NotFoundMemoException(id)
         val commentsResponse = CommentsResponse.of(commentQueryRepository.readCommentsInMemo(id, request))
         return MemoResponse.of(memo, commentsResponse)
+    }
+
+    @Transactional(readOnly = true)
+    fun readMemoWithUsername(userDetails: UserDetails, id: Long, request: CommonPageRequest): MemoResponse {
+        val username = userDetails.username
+        val memo = memoQueryRepository.findByIdOrNullWithUsername(id, username)
+            ?: throw NotFoundMemoException(id)
+        val commentsResponse = CommentsResponse.of(
+            commentQueryRepository.readCommentsInMemoWithUsername(id, username, request)
+        )
+        return MemoResponse.of(memo, commentsResponse)
+    }
+
+    @Transactional
+    fun deleteMemo(userDetails: UserDetails, id: Long) {
+        val memo = memoRepository.findByIdOrNull(id) ?: throw NotFoundMemoException(id)
+        validate(userDetails.username, memo.writer.username)
+        memoLikeRepository.deleteAllByIdInBatch(memoLikeRepository.findAllByMemoId(id))
+        memoRepository.delete(memo)
     }
 
     @Transactional(readOnly = true)
@@ -64,5 +81,28 @@ class MemoService(
     @Transactional(readOnly = true)
     fun readMemosInUpdatedOrder(size: Int): MemosResponse {
         return MemosResponse.of(memoQueryRepository.findMemosInUpdatedOrder(size))
+    }
+
+    @Transactional
+    fun increaseMemoLikeCount(userDetails: UserDetails, id: Long, username: String): Long {
+        validate(userDetails.username, username)
+        val memo = memoRepository.findByIdOrNull(id) ?: throw NotFoundMemoException(id)
+        val member = memberRepository.findByUsername(username) ?: throw NotFoundUsernameException(username)
+        return memoLikeRepository.save(MemoLike.create(member, memo)).id!!
+    }
+
+    @Transactional
+    fun decreaseMemoLikeCount(userDetails: UserDetails, id: Long, username: String) {
+        validate(userDetails.username, username)
+        val memoLike = memoLikeRepository.findByMemoIdAndMemberUsername(id, username)
+            ?: throw NotFoundMemoLikeException(id, username)
+        memoLike.remove()
+        return memoLikeRepository.delete(memoLike)
+    }
+
+    private fun validate(tokenUsername: String, reqUsername: String) {
+        if (tokenUsername != reqUsername) {
+            throw CommonException(INVALID_JWT_TOKEN)
+        }
     }
 }

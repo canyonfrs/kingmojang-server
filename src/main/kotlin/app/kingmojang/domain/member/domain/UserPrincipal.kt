20f5a73ca2e967e64b539cee4412cf30890a7fc7
SeoSeunghun <proto_seo@naver.com>
2023-03-27 23:19:19 +0900

    feat: add Oauth2User implementation

--- src/main/kotlin/app/kingmojang/domain/member/domain/UserPrincipal.kt
+++ src/main/kotlin/app/kingmojang/domain/member/domain/UserPrincipal.kt
@@ -3,14 +3,37 @@ package app.kingmojang.domain.member.domain
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.oauth2.core.user.OAuth2User
 
-class UserDetailsImpl(val member: Member) : UserDetails {
-
+class UserPrincipal(
+    private val member: Member,
+    private val attributes: MutableMap<String, Any>,
+) : UserDetails, OAuth2User {
     private var enabled = !member.deleted
+
+    companion object {
+        fun create(member: Member): UserPrincipal {
+            return UserPrincipal(member, mutableMapOf())
+        }
+
+        fun create(member: Member, attributes: MutableMap<String, Any>): UserPrincipal {
+            return UserPrincipal(member, attributes)
+        }
+    }
 
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
         return mutableListOf(SimpleGrantedAuthority(member.type.value))
     }
+
+    fun getId(): Long = member.id!!
+
+    fun getNickname(): String = member.nickname
+
+    fun getProvider(): String = member.provider.toString()
+
+    override fun getName(): String = member.username
+
+    override fun getAttributes(): MutableMap<String, Any> = this.attributes
 
     override fun getPassword(): String = member.password
 

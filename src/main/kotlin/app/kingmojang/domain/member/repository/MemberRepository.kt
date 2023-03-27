143eb42ad8ffecdeaaccdcb1487780ea1ed2e283
SeoSeunghun <proto_seo@naver.com>
2023-03-27 23:27:56 +0900

    feat: add findByEmail

--- src/main/kotlin/app/kingmojang/domain/member/repository/MemberRepository.kt
+++ src/main/kotlin/app/kingmojang/domain/member/repository/MemberRepository.kt
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface MemberRepository : JpaRepository<Member, Long> {
     fun findByUsername(username: String): Member?
+    fun findByEmail(username: String): Member?
     fun existsByUsername(username: String): Boolean
     fun existsByNicknameAndType(nickname: String, type: MemberType): Boolean
     fun existsByEmail(email: String): Boolean

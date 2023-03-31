f157feacb1f6e797feb9d1933dede10f0d6a507d
SeoSeunghun <proto_seo@naver.com>
2023-04-01 00:04:25 +0900

    refactor: replace username to memberId

--- src/main/kotlin/app/kingmojang/domain/comment/dto/request/ReplyRequest.kt
+++ src/main/kotlin/app/kingmojang/domain/comment/dto/request/ReplyRequest.kt
@@ -2,9 +2,10 @@ package app.kingmojang.domain.comment.dto.request
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
 data class ReplyRequest(
-    @field:NotBlank val username: String,
+    @field:NotNull val memberId: Long,
     @field:NotBlank val content: String,
     @JsonInclude(JsonInclude.Include.NON_NULL) val emojiId: Long,
 ) {

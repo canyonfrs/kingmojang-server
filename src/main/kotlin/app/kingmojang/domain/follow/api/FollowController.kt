04b28ed83e71e8f65f3feebb215e81fd2c5862d3
SeoSeunghun <proto_seo@naver.com>
2023-04-01 00:05:11 +0900

    refactor: replace UserDetails to UserPrincipal

--- src/main/kotlin/app/kingmojang/domain/follow/api/FollowController.kt
+++ src/main/kotlin/app/kingmojang/domain/follow/api/FollowController.kt
@@ -1,11 +1,11 @@
 package app.kingmojang.domain.follow.api
 
 import app.kingmojang.domain.follow.application.FollowService
+import app.kingmojang.domain.member.domain.UserPrincipal
 import app.kingmojang.global.common.response.CommonResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
@@ -17,28 +17,28 @@ class FollowController(
     @PostMapping("/follows/{followerId}/to/{creatorId}")
     @PreAuthorize("hasRole({'USER', 'CREATOR'})")
     fun createFollow(
-        @AuthenticationPrincipal userDetails: UserDetails,
-        @RequestParam followerId: Long,
-        @RequestParam creatorId: Long,
+        @AuthenticationPrincipal userPrincipal: UserPrincipal,
+        @PathVariable followerId: Long,
+        @PathVariable creatorId: Long,
     ): ResponseEntity<CommonResponse<Long>> {
-        val followId = followService.createFollow(userDetails, followerId, creatorId)
+        val followId = followService.createFollow(userPrincipal, followerId, creatorId)
 
         val uri = ServletUriComponentsBuilder
             .fromCurrentContextPath().path("/follows/${followId}")
             .buildAndExpand().toUri()
 
         return ResponseEntity.created(uri).body(CommonResponse.success(followId))
-
     }
 
     @DeleteMapping("/follows/{followerId}/to/{creatorId}")
     @PreAuthorize("hasRole({'USER', 'CREATOR'})")
     fun deleteFollow(
-        @AuthenticationPrincipal userDetails: UserDetails,
-        @RequestParam followerId: Long,
-        @RequestParam creatorId: Long,
-    ) {
-        followService.deleteFollow(userDetails, followerId, creatorId)
+        @AuthenticationPrincipal userPrincipal: UserPrincipal,
+        @PathVariable followerId: Long,
+        @PathVariable creatorId: Long,
+    ): ResponseEntity<CommonResponse<Void>> {
+        followService.deleteFollow(userPrincipal, followerId, creatorId)
+        return ResponseEntity.ok(CommonResponse.success())
     }
 
 }

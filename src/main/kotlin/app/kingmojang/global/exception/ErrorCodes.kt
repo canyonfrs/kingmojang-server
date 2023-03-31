614892bc4cde5d95a59d15bb65c045568fc8b4d7
SeoSeunghun <proto_seo@naver.com>
2023-04-01 00:10:49 +0900

    feat: add ErrorCodes, add BadCredentialsException handler

--- src/main/kotlin/app/kingmojang/global/exception/ErrorCodes.kt
+++ src/main/kotlin/app/kingmojang/global/exception/ErrorCodes.kt
@@ -15,9 +15,11 @@ enum class ErrorCodes(val status: HttpStatus, val description: String) {
     NOT_FOUND_LIKE(HttpStatus.NOT_FOUND, ""),
     NOT_FOUND_FOLLOW(HttpStatus.NOT_FOUND, ""),
     UNHANDLED_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, ""),
+    BAD_CREDENTIAL_EXCEPTION(HttpStatus.UNAUTHORIZED, ""),
     INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
     INVALID_JWT_SIGNATURE(HttpStatus.UNAUTHORIZED, "토큰의 시그니처가 잘못되었습니다."),
     UNSUPPORTED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "지원하지 않는 JWT 토큰 입니다."),
     EXPIRED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "Access 토큰이 만료되었습니다."),
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "Refresh 토큰이 만료되었습니다."),
+    INVALID_REDIRECT_URI(HttpStatus.INTERNAL_SERVER_ERROR, ""),
 }

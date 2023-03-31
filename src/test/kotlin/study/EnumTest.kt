b55745c0be55d8eb79c6d8ecb3c2fe8c308cfe00
SeoSeunghun <proto_seo@naver.com>
2023-04-01 00:01:05 +0900

    test: add valueOf Method Test

--- src/test/kotlin/study/EnumTest.kt
+++ src/test/kotlin/study/EnumTest.kt
@@ -10,6 +10,11 @@ class EnumTest {
         Assertions.assertThat(HelloEnum.A.name).isEqualTo("A")
         Assertions.assertThat(HelloEnum.A.value).isEqualTo("a")
     }
+
+    @Test
+    fun `valueOf Test`() {
+        Assertions.assertThat(HelloEnum.valueOf("a".uppercase())).isEqualTo(HelloEnum.A)
+    }
 }
 enum class HelloEnum(val value: String) {
     A("a"), B("b")

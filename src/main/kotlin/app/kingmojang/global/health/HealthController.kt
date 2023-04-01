63439baf2f1f5170c9afc353f43ffea2bec75e61
SeoSeunghun <proto_seo@naver.com>
2023-04-01 09:46:37 +0900

    feat: add Health check api

---
+++ src/main/kotlin/app/kingmojang/global/health/HealthController.kt
@@ -0,0 +1,13 @@
+package app.kingmojang.global.health
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1")
+class HealthController {
+
+    @GetMapping("/health")
+    fun health(): String = "OK"
+}

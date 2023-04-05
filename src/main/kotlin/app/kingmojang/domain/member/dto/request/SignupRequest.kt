43fc5fb092458b0af8b2214c78eeb81589e9143f
SeoSeunghun <proto_seo@naver.com>
2023-04-06 00:48:05 +0900

    refactor: remove NotBlank valid annotation to password

--- src/main/kotlin/app/kingmojang/domain/member/dto/request/SignupRequest.kt
+++ src/main/kotlin/app/kingmojang/domain/member/dto/request/SignupRequest.kt
@@ -2,19 +2,13 @@ package app.kingmojang.domain.member.dto.request
 
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
 
 data class SignupRequest(
     @field:NotBlank val username: String,
-    @field:NotBlank val password: String,
+    val password: String = "",
     @field:NotBlank val nickname: String,
     @field:Email val email: String,
-    @field:NotNull val isAuthorizedAccount: Boolean,
     @field:NotBlank val provider: String,
-    val introduce: String?,
-    val youtube: String?,
-    val broadcastLink: String?,
-    val donationLink: String?,
     @field:NotBlank val memberType: String,
 ) {
 }

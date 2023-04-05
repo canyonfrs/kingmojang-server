df2ff6022de6edc983373a362dc44d703010a82a
SeoSeunghun <proto_seo@naver.com>
2023-04-06 00:32:05 +0900

    refactor: modify code that used username to use memberId

--- src/main/kotlin/app/kingmojang/domain/member/dto/request/RefreshRequest.kt
+++ src/main/kotlin/app/kingmojang/domain/member/dto/request/RefreshRequest.kt
@@ -1,9 +1,10 @@
 package app.kingmojang.domain.member.dto.request
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
 data class RefreshRequest(
     @field:NotBlank val refreshToken: String,
-    @field:NotBlank val username: String,
+    @field:NotNull val memberId: Long,
 ) {
 }

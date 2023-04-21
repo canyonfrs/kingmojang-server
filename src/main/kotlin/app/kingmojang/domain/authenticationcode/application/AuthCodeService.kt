6729270dd88186f8f54498fdaecd435dc42bf2cc
SeoSeunghun <proto_seo@naver.com>
2023-04-21 23:03:37 +0900

    feat: add authentication code service

---
+++ src/main/kotlin/app/kingmojang/domain/authenticationcode/application/AuthCodeService.kt
@@ -0,0 +1,9 @@
+package app.kingmojang.domain.authenticationcode.application
+
+import app.kingmojang.domain.authenticationcode.dto.response.AuthCodeResponse
+import app.kingmojang.domain.member.domain.UserPrincipal
+
+interface AuthCodeService {
+    fun createAuthCode(userPrincipal: UserPrincipal, email: String): AuthCodeResponse
+    fun sendAuthCodeToEmail(response: AuthCodeResponse)
+}

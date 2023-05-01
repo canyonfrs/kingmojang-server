ef5187b2632c80e29c9c4736bf16c3d5cab0d04e
SeoSeunghun <proto_seo@naver.com>
2023-05-01 19:55:05 +0900

    test: add username, nickname, email duplicate check api test

---
+++ src/test/kotlin/app/kingmojang/api/member/MemberControllerTest.kt
@@ -0,0 +1,91 @@
+package app.kingmojang.api.member
+
+import app.kingmojang.api.RestControllerTest
+import app.kingmojang.domain.member.api.MemberController
+import app.kingmojang.domain.member.application.MemberService
+import app.kingmojang.domain.memo.application.MemoService
+import app.kingmojang.fixture.*
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.get
+
+@WebMvcTest(MemberController::class)
+class MemberControllerTest : RestControllerTest() {
+    @MockkBean
+    private lateinit var memberService: MemberService
+
+    @MockkBean
+    private lateinit var memoService: MemoService
+
+    @Test
+    fun `중복된 아이디가 없는 경우 204 응답코드가 반환된다`() {
+        every { memberService.existsUsername(USERNAME) } returns false
+
+        mockMvc.get("/api/v1/members/username") {
+            param("username", USERNAME)
+        }.andExpect {
+            status { isNoContent() }
+        }
+    }
+
+    @Test
+    fun `중복된 아이디가 있는 경우 409 응답코드가 반환된다`() {
+        every { memberService.existsUsername(USERNAME) } returns true
+
+        mockMvc.get("/api/v1/members/username") {
+            param("username", USERNAME)
+        }.andExpect {
+            status { isConflict() }
+            content { success() }
+        }
+    }
+
+    @Test
+    fun `중복된 닉네임이 없는 경우 204 응답코드가 반환된다`() {
+        every { memberService.existsNickname(NICKNAME, any()) } returns false
+
+        mockMvc.get("/api/v1/members/nickname") {
+            param("nickname", NICKNAME)
+        }.andExpect {
+            status { isNoContent() }
+        }
+    }
+
+    @Test
+    fun `중복된 닉네임이 있는 경우 409 응답코드가 반환된다`() {
+        every { memberService.existsNickname(NICKNAME, any()) } returns true
+
+        mockMvc.get("/api/v1/members/nickname") {
+            param("nickname", NICKNAME)
+        }.andExpect {
+            status { isConflict() }
+            content { success() }
+        }
+    }
+
+    @Test
+    fun `중복된 이메일이 없는 경우 204 응답코드가 반환된다`() {
+        every { memberService.existsEmail(EMAIL) } returns false
+
+        mockMvc.get("/api/v1/members/email") {
+            param("email", EMAIL)
+        }.andExpect {
+            status { isNoContent() }
+        }
+    }
+
+    @Test
+    fun `중복된 이메일이 있는 경우 409 응답코드가 반환된다`() {
+        every { memberService.existsEmail(EMAIL) } returns true
+
+        mockMvc.get("/api/v1/members/email") {
+            param("email", EMAIL)
+        }.andExpect {
+            status { isConflict() }
+            content { success() }
+        }
+    }
+
+}

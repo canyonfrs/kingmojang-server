f1d8623e97a718fc106afb3da286d68796211d93
SeoSeunghun <proto_seo@naver.com>
2023-05-01 19:55:25 +0900

    feat: add RestControllerTest

---
+++ src/test/kotlin/app/kingmojang/api/RestControllerTest.kt
@@ -0,0 +1,74 @@
+package app.kingmojang.api
+
+import app.kingmojang.global.common.response.CommonResponse
+import app.kingmojang.global.security.JwtUtils
+import app.kingmojang.global.security.UserDetailsServiceImpl
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ninjasquad.springmockk.MockkBean
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.http.MediaType
+import org.springframework.restdocs.RestDocumentationContextProvider
+import org.springframework.restdocs.RestDocumentationExtension
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
+import org.springframework.restdocs.operation.preprocess.Preprocessors
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockHttpServletRequestDsl
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultMatcher
+import org.springframework.test.web.servlet.result.ContentResultMatchers
+import org.springframework.test.web.servlet.result.ContentResultMatchersDsl
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+import org.springframework.web.filter.CharacterEncodingFilter
+
+@AutoConfigureRestDocs
+@ExtendWith(RestDocumentationExtension::class)
+@ActiveProfiles("test")
+abstract class RestControllerTest {
+    @MockkBean
+    lateinit var jwtUtils: JwtUtils
+
+    @MockkBean
+    lateinit var userDetailsServiceImpl: UserDetailsServiceImpl
+
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+
+    lateinit var mockMvc: MockMvc
+
+    @BeforeEach
+    internal fun setUp(
+        webApplicationContext: WebApplicationContext,
+        restDocumentationContextProvider: RestDocumentationContextProvider
+    ) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .addFilter<DefaultMockMvcBuilder>(CharacterEncodingFilter("UTF-8", true))
+            .alwaysDo<DefaultMockMvcBuilder>(MockMvcResultHandlers.print())
+            .apply<DefaultMockMvcBuilder>(
+                MockMvcRestDocumentation.documentationConfiguration(
+                    restDocumentationContextProvider
+                ).operationPreprocessors()
+                    .withRequestDefaults(Preprocessors.prettyPrint())
+                    .withResponseDefaults(Preprocessors.prettyPrint())
+            )
+            .build()
+    }
+
+    fun MockHttpServletRequestDsl.jsonContent(value: Any) {
+        content = objectMapper.writeValueAsString(value)
+        contentType = MediaType.APPLICATION_JSON
+    }
+
+    fun ContentResultMatchersDsl.success() {
+        json(objectMapper.writeValueAsString(CommonResponse.success()), true)
+    }
+
+    fun ContentResultMatchersDsl.success(value: Any) {
+        json(objectMapper.writeValueAsString(CommonResponse.success(value)), true)
+    }
+}

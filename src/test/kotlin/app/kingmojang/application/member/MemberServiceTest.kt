aeb3affbe1c334b75f51b5cf343a9122c02ed765
SeoSeunghun <proto_seo@naver.com>
2023-05-10 00:09:49 +0900

    test: add memberService test

---
+++ src/test/kotlin/app/kingmojang/application/member/MemberServiceTest.kt
@@ -0,0 +1,79 @@
+package app.kingmojang.application.member
+
+import app.kingmojang.domain.member.application.MemberService
+import app.kingmojang.domain.member.domain.MemberType
+import app.kingmojang.domain.member.repository.MemberRepository
+import app.kingmojang.fixture.EMAIL
+import app.kingmojang.fixture.NICKNAME
+import app.kingmojang.fixture.USERNAME
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+
+class MemberServiceTest : BehaviorSpec({
+    val memberRepository = mockk<MemberRepository>()
+
+    val memberService = MemberService(memberRepository)
+
+    Given("중복되지 않은 아이디가 주어진 경우") {
+        every { memberRepository.existsByUsername(USERNAME) } returns false
+        When("해당 아이디가 존재하는지 확인하면") {
+
+            Then("false가 반환된다") {
+                memberService.existsUsername(USERNAME) shouldBe false
+            }
+        }
+    }
+
+    Given("중복된 아이디가 주어진 경우") {
+        every { memberRepository.existsByUsername(USERNAME) } returns true
+        When("해당 아이디가 존재하는지 확인하면") {
+
+            Then("true가 반환된다") {
+                memberService.existsUsername(USERNAME) shouldBe true
+            }
+        }
+    }
+
+    Given("중복되지 않은 닉네임이 주어진 경우") {
+        every { memberRepository.existsByNicknameAndType(NICKNAME, MemberType.USER) } returns false
+        When("해당 닉네임이 존재하는지 확인하면") {
+
+            Then("false가 반환된다") {
+                memberService.existsNickname(NICKNAME, "USER") shouldBe false
+            }
+        }
+    }
+
+    Given("중복된 닉네임이 주어진 경우") {
+        every { memberRepository.existsByNicknameAndType(NICKNAME, MemberType.USER) } returns true
+        When("해당 닉네임이 존재하는지 확인하면") {
+
+            Then("true가 반환된다") {
+                memberService.existsNickname(NICKNAME, "USER") shouldBe true
+            }
+        }
+    }
+
+    Given("중복되지 않은 이메일이 주어진 경우") {
+        every { memberRepository.existsByEmail(EMAIL) } returns false
+        When("해당 이메일이 존재하는지 확인하면") {
+
+            Then("false가 반환된다") {
+                memberService.existsEmail(EMAIL) shouldBe false
+            }
+        }
+    }
+
+    Given("중복된 이메일이 주어진 경우") {
+        every { memberRepository.existsByEmail(EMAIL) } returns true
+        When("해당 이메일이 존재하는지 확인하면") {
+
+            Then("true가 반환된다") {
+                memberService.existsEmail(EMAIL) shouldBe true
+            }
+        }
+    }
+
+})

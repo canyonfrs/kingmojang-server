363e12f44905b029a116918dd67be1f53358c145
SeoSeunghun <proto_seo@naver.com>
2023-05-26 18:37:26 +0900

    feat: add highlight domain

---
+++ src/main/kotlin/app/kingmojang/domain/highlight/domain/Highlight.kt
@@ -0,0 +1,29 @@
+package app.kingmojang.domain.highlight.domain
+
+import jakarta.persistence.*
+
+@Entity
+class Highlight(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "highlight_id")
+    val id: Long? = null,
+
+    @Lob
+    val content: String,
+
+    @Column(name = "start_x")
+    val startX: Int,
+
+    @Column(name = "start_y")
+    val startY: Int,
+
+    @Column(name = "end_x")
+    val endX: Int,
+
+    @Column(name = "end_y")
+    val endY: Int,
+
+    val version: Int,
+) {
+}

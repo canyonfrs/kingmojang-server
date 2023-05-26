e96f03f3ae1b2863054c1e3bbb849e60daae27c6
SeoSeunghun <proto_seo@naver.com>
2023-05-26 19:14:45 +0900

    feat: change username exception to email exception

--- src/main/kotlin/app/kingmojang/domain/member/exception/NotFoundEmailException.kt
+++ src/main/kotlin/app/kingmojang/domain/member/exception/NotFoundEmailException.kt
@@ -3,10 +3,10 @@ package app.kingmojang.domain.member.exception
 import app.kingmojang.global.exception.CommonException
 import app.kingmojang.global.exception.ErrorCodes
 
-class NotFoundUsernameException(
-    private val username: String,
-    override val message: String = "User with username('$username') not found.",
+class NotFoundEmailException(
+    private val email: String,
+    override val message: String = "User with email('$email') not found.",
     override val cause: Throwable? = null,
 ) : CommonException(ErrorCodes.NOT_FOUND_USERNAME, message, cause) {
-    override fun messageArguments(): Collection<String> = setOf(username)
+    override fun messageArguments(): Collection<String> = setOf(email)
 }

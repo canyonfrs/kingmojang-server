9924ebd38b31a7e8f4a481e5b6e414cb2bd92e20
SeoSeunghun <proto_seo@naver.com>
2023-05-26 19:15:13 +0900

    feat: remove username, chagne username to email

--- src/main/kotlin/app/kingmojang/domain/member/dto/request/LoginRequest.kt
+++ src/main/kotlin/app/kingmojang/domain/member/dto/request/LoginRequest.kt
@@ -3,7 +3,7 @@ package app.kingmojang.domain.member.dto.request
 import jakarta.validation.constraints.NotBlank
 
 data class LoginRequest(
-    @field:NotBlank val username: String,
+    @field:NotBlank val email: String,
     @field:NotBlank val password: String,
 ) {
 }

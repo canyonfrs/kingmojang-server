b55b120807a73a8ffda7d0b5367c390c5e8fdb04
SeoSeunghun <proto_seo@naver.com>
2023-06-06 18:14:12 +0900

    feat: add S3 Util to upload and delete image

---
+++ src/main/kotlin/app/kingmojang/global/util/S3Utils.kt
@@ -0,0 +1,30 @@
+package app.kingmojang.global.util
+
+import io.awspring.cloud.s3.ObjectMetadata
+import io.awspring.cloud.s3.S3Operations
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.web.multipart.MultipartFile
+
+@Component
+class S3Utils(
+    @Value("\${spring.cloud.aws.s3.bucket}") private val bucket: String,
+    private val s3Operations: S3Operations
+) {
+
+    fun uploadFile(fileName: String, file: MultipartFile): String {
+        file.inputStream.use { inputStream ->
+            val resource = s3Operations.upload(
+                bucket,
+                fileName,
+                inputStream,
+                ObjectMetadata.builder().contentType(file.contentType).build()
+            )
+            return resource.url.toString()
+        }
+    }
+
+    fun deleteFile(key: String) {
+        s3Operations.deleteObject(bucket, key)
+    }
+}

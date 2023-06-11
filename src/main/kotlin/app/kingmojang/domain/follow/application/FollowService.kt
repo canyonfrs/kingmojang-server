dd7fa8165aae1d281d3a945b829e59cb7c12083e
SeoSeunghun <proto_seo@naver.com>
2023-06-11 21:08:51 +0900

    feat: read members followers

--- src/main/kotlin/app/kingmojang/domain/follow/application/FollowService.kt
+++ src/main/kotlin/app/kingmojang/domain/follow/application/FollowService.kt
@@ -1,13 +1,17 @@
 package app.kingmojang.domain.follow.application
 
 import app.kingmojang.domain.follow.domain.Follow
+import app.kingmojang.domain.follow.dto.response.FollowResponse
 import app.kingmojang.domain.follow.exception.FollowAlreadyExistException
 import app.kingmojang.domain.follow.exception.NotFoundFollowException
 import app.kingmojang.domain.follow.repository.FollowRepository
 import app.kingmojang.domain.member.domain.UserPrincipal
 import app.kingmojang.domain.member.exception.NotFoundMemberException
 import app.kingmojang.domain.member.repository.MemberRepository
+import app.kingmojang.global.common.request.CommonPageRequest
 import app.kingmojang.global.validator.MemberIdValidator
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -19,7 +23,7 @@ class FollowService(
 ) {
 
     @Transactional
-    fun createFollow(userPrincipal: UserPrincipal , followerId: Long, creatorId: Long): Long {
+    fun createFollow(userPrincipal: UserPrincipal, followerId: Long, creatorId: Long): Long {
         MemberIdValidator.validate(userPrincipal.getId(), followerId)
         if (followRepository.existsByCreatorIdAndFollowerId(creatorId, followerId)) {
             throw FollowAlreadyExistException(followerId, creatorId)
@@ -36,5 +40,13 @@ class FollowService(
             ?: throw NotFoundFollowException(followerId, creatorId))
         follow.remove()
         followRepository.delete(follow)
+    }
+
+    @Transactional(readOnly = true)
+    fun readFollowByFollower(memberId: Long, request: CommonPageRequest): List<FollowResponse> {
+        return followRepository.findAllByFollowerId(
+            memberId,
+            PageRequest.of(request.page.toInt(), request.size.toInt(), Sort.by("createdAt").descending())
+        ).map { FollowResponse.of(it) }
     }
 }

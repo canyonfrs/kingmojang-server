725ebbd97b8d3b5c91c67eec1408cb9bfb3344ca
SeoSeunghun <proto_seo@naver.com>
2023-06-21 23:37:49 +0900

    feat: update comment, reply service

--- src/main/kotlin/app/kingmojang/domain/comment/application/CommentService.kt
+++ src/main/kotlin/app/kingmojang/domain/comment/application/CommentService.kt
@@ -4,79 +4,75 @@ import app.kingmojang.domain.comment.domain.Comment
 import app.kingmojang.domain.comment.dto.request.CommentRequest
 import app.kingmojang.domain.comment.dto.response.CommentsResponse
 import app.kingmojang.domain.comment.exception.NotFoundCommentException
-import app.kingmojang.domain.comment.repository.CommentQueryRepository
 import app.kingmojang.domain.comment.repository.CommentRepository
 import app.kingmojang.domain.like.domain.CommentLike
 import app.kingmojang.domain.like.exception.NotFoundCommentLikeException
 import app.kingmojang.domain.like.repository.CommentLikeRepository
-import app.kingmojang.domain.member.domain.UserPrincipal
 import app.kingmojang.domain.member.exception.NotFoundMemberException
 import app.kingmojang.domain.member.repository.MemberRepository
 import app.kingmojang.domain.memo.exception.NotFoundMemoException
 import app.kingmojang.domain.memo.repository.MemoRepository
-import app.kingmojang.global.common.request.CommonPageRequest
-import app.kingmojang.global.validator.MemberIdValidator
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Slice
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class CommentService(
     private val memberRepository: MemberRepository,
     private val memoRepository: MemoRepository,
     private val commentRepository: CommentRepository,
-    private val commentQueryRepository: CommentQueryRepository,
     private val commentLikeRepository: CommentLikeRepository,
 ) {
 
-    @Transactional
-    fun createComment(userPrincipal: UserPrincipal, memoId: Long, request: CommentRequest): Long {
-        val memberId = request.memberId
-        MemberIdValidator.validate(userPrincipal.getId(), memberId)
+    fun createComment(memoId: Long, memberId: Long, request: CommentRequest): Long {
         val writer = memberRepository.findByIdOrNull(memberId) ?: throw NotFoundMemberException(memberId)
         val memo = memoRepository.findByIdOrNull(memoId) ?: throw NotFoundMemoException(memoId)
         return commentRepository.save(Comment.create(writer, memo, request)).id!!
     }
 
-    @Transactional
-    fun updateComment(userPrincipal: UserPrincipal, commentId: Long, request: CommentRequest): Long {
-        MemberIdValidator.validate(userPrincipal.getId(), request.memberId)
+    fun updateComment(commentId: Long, memberId: Long, request: CommentRequest): Long {
         val comment = commentRepository.findByIdOrNull(commentId) ?: throw NotFoundCommentException(commentId)
-        return comment.update(request).id!!
+        return comment.update(memberId, request).id!!
     }
 
-    @Transactional
-    fun deleteComment(userPrincipal: UserPrincipal, commentId: Long) {
+    fun deleteComment(commentId: Long, memberId: Long) {
         val comment = commentRepository.findByIdOrNull(commentId) ?: throw NotFoundCommentException(commentId)
-        MemberIdValidator.validate(userPrincipal.getId(), comment.writer.id!!)
         commentLikeRepository.deleteAllByIdInBatch(commentLikeRepository.findAllByCommentId(commentId))
-        comment.remove()
+        comment.remove(memberId)
     }
 
     @Transactional(readOnly = true)
-    fun readComments(memoId: Long, request: CommonPageRequest): CommentsResponse {
-        return CommentsResponse.of(commentQueryRepository.readCommentsInMemo(memoId, request))
+    fun readComments(memoId: Long, memberId: Long?, request: PageRequest): CommentsResponse {
+        val comments = commentRepository.findAllWithWriterByMemoIdAndDeletedFalse(memoId, request)
+        val commentLikes = if (memberId != null) extractCommentsLike(comments, memberId) else emptyMap()
+        return CommentsResponse.of(comments, commentLikes)
     }
 
-    @Transactional
-    fun increaseCommentLikeCount(userPrincipal: UserPrincipal, id: Long, memberId: Long): Long {
-        MemberIdValidator.validate(userPrincipal.getId(), memberId)
+    fun increaseCommentLikeCount(commentId: Long, memberId: Long): Long {
         val member = memberRepository.findByIdOrNull(memberId) ?: throw NotFoundMemberException(memberId)
-        val comment = commentRepository.findByIdOrNull(id) ?: throw NotFoundCommentException(id)
+        val comment = commentRepository.findByIdOrNull(commentId) ?: throw NotFoundCommentException(commentId)
         return commentLikeRepository.save(CommentLike.create(member, comment)).id!!
     }
 
-    @Transactional
-    fun decreaseCommentLikeCount(userPrincipal: UserPrincipal, id: Long, memberId: Long) {
-        MemberIdValidator.validate(userPrincipal.getId(), memberId)
-        val commentLike = commentLikeRepository.findByCommentIdAndMemberId(id, memberId)
-            ?: throw NotFoundCommentLikeException(id, memberId)
+    fun decreaseCommentLikeCount(commentId: Long, memberId: Long) {
+        val commentLike = commentLikeRepository.findByCommentIdAndMemberId(commentId, memberId)
+            ?: throw NotFoundCommentLikeException(commentId, memberId)
         commentLike.remove()
         commentLikeRepository.delete(commentLike)
     }
 
     @Transactional(readOnly = true)
-    fun readCommentsByMember(memberId: Long, request: CommonPageRequest): CommentsResponse {
-        return CommentsResponse.of(commentQueryRepository.readCommentsByMember(memberId, request))
+    fun readCommentsByMember(memberId: Long, request: PageRequest): CommentsResponse {
+        val comments = commentRepository.findAllWithWriterByWriterIdAndDeletedFalse(memberId, request)
+        return CommentsResponse.of(comments, emptyMap())
+    }
+
+    private fun extractCommentsLike(comments: Slice<Comment>, memberId: Long): Map<Long, CommentLike> {
+        val commentIds = comments.content.map { it.id!! }
+        return commentLikeRepository.findAllByCommentIdInAndMemberId(commentIds, memberId)
+            .associateBy { it.id!! }
     }
 }
--- src/main/kotlin/app/kingmojang/domain/comment/application/ReplyService.kt
+++ src/main/kotlin/app/kingmojang/domain/comment/application/ReplyService.kt
@@ -2,6 +2,7 @@ package app.kingmojang.domain.comment.application
 
 import app.kingmojang.domain.comment.domain.Reply
 import app.kingmojang.domain.comment.dto.request.ReplyRequest
+import app.kingmojang.domain.comment.dto.response.RepliesResponse
 import app.kingmojang.domain.comment.exception.NotFoundCommentException
 import app.kingmojang.domain.comment.exception.NotFoundReplyException
 import app.kingmojang.domain.comment.repository.CommentRepository
@@ -9,17 +10,17 @@ import app.kingmojang.domain.comment.repository.ReplyRepository
 import app.kingmojang.domain.like.domain.ReplyLike
 import app.kingmojang.domain.like.exception.NotFoundReplyLikeException
 import app.kingmojang.domain.like.repository.ReplyLikeRepository
-import app.kingmojang.domain.member.domain.UserPrincipal
 import app.kingmojang.domain.member.exception.NotFoundMemberException
 import app.kingmojang.domain.member.repository.MemberRepository
 import app.kingmojang.domain.memo.exception.NotFoundMemoException
 import app.kingmojang.domain.memo.repository.MemoRepository
-import app.kingmojang.global.validator.MemberIdValidator
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class ReplyService(
     private val memberRepository: MemberRepository,
     private val memoRepository: MemoRepository,
@@ -27,45 +28,50 @@ class ReplyService(
     private val replyRepository: ReplyRepository,
     private val replyLikeRepository: ReplyLikeRepository,
 ) {
-    @Transactional
-    fun createReply(userPrincipal: UserPrincipal, memoId: Long, commentId: Long, request: ReplyRequest): Long {
-        val memberId = request.memberId
-        MemberIdValidator.validate(userPrincipal.getId(), memberId)
+
+    fun createReply(memoId: Long, commentId: Long, memberId: Long, request: ReplyRequest): Long {
         val writer = memberRepository.findByIdOrNull(memberId) ?: throw NotFoundMemberException(memberId)
         val memo = memoRepository.findByIdOrNull(memoId) ?: throw NotFoundMemoException(memoId)
         val comment = commentRepository.findByIdOrNull(commentId) ?: throw NotFoundCommentException(commentId)
         return replyRepository.save(Reply.create(writer, memo, comment, request)).id!!
     }
 
-    @Transactional
-    fun updateReply(userPrincipal: UserPrincipal, replyId: Long, request: ReplyRequest): Long {
-        MemberIdValidator.validate(userPrincipal.getId(), request.memberId)
+    fun updateReply(replyId: Long, memberId: Long, request: ReplyRequest): Long {
         val reply = replyRepository.findByIdOrNull(replyId) ?: throw NotFoundReplyException(replyId)
-        return reply.update(request).id!!
+        return reply.update(memberId, request).id!!
     }
 
-    @Transactional
-    fun deleteReply(userPrincipal: UserPrincipal, replyId: Long) {
+    fun deleteReply(replyId: Long, memberId: Long) {
         val reply = replyRepository.findByIdOrNull(replyId) ?: throw NotFoundReplyException(replyId)
-        MemberIdValidator.validate(userPrincipal.getId(), reply.writer.id!!)
-        reply.remove()
+        reply.remove(memberId)
         replyLikeRepository.deleteAllByIdInBatch(replyLikeRepository.findAllByReplyId(replyId))
     }
 
-    @Transactional
-    fun increaseReplyLikeCount(userPrincipal: UserPrincipal, replyId: Long, memberId: Long): Long {
-        MemberIdValidator.validate(userPrincipal.getId(), memberId)
+    fun increaseReplyLikeCount(replyId: Long, memberId: Long): Long {
         val member = memberRepository.findByIdOrNull(memberId) ?: throw NotFoundMemberException(memberId)
         val reply = replyRepository.findByIdOrNull(replyId) ?: throw NotFoundReplyException(replyId)
         return replyLikeRepository.save(ReplyLike.create(member, reply)).id!!
     }
 
-    @Transactional
-    fun decreaseReplyLikeCount(userPrincipal: UserPrincipal, replyId: Long, memberId: Long) {
-        MemberIdValidator.validate(userPrincipal.getId(), memberId)
+    fun decreaseReplyLikeCount(replyId: Long, memberId: Long) {
         val reply = replyLikeRepository.findByReplyIdAndMemberId(replyId, memberId)
             ?: throw NotFoundReplyLikeException(replyId, memberId)
         reply.remove()
         replyLikeRepository.delete(reply)
+    }
+
+    @Transactional(readOnly = true)
+    fun readReplies(commentId: Long, memberId: Long?, request: PageRequest): RepliesResponse {
+        val replies = replyRepository.findAllWithWriterByCommentIdAndDeletedFalse(commentId, request)
+        val replyIds = replies.content.map { it.id!! }
+        val repliesLikes = if (memberId != null) replyLikeRepository.findAllByReplyIdInAndMemberId(replyIds, memberId)
+            .associateBy { it.reply.id!! } else emptyMap()
+        return RepliesResponse.of(replies, repliesLikes)
+    }
+
+    @Transactional(readOnly = true)
+    fun readRepliesByMember(memberId: Long, request: PageRequest) : RepliesResponse {
+        val replies = replyRepository.findAllWithWriterByWriterIdAndDeletedFalse(memberId, request)
+        return RepliesResponse.of(replies, emptyMap())
     }
 }

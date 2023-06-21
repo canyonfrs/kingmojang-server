bcebdb66829faa71a8bf6cf3e95f0d444589d3d5
SeoSeunghun <proto_seo@naver.com>
2023-06-21 23:45:36 +0900

    feat: update follow repository

--- src/main/kotlin/app/kingmojang/domain/follow/repository/FollowRepository.kt
+++ src/main/kotlin/app/kingmojang/domain/follow/repository/FollowRepository.kt
@@ -2,6 +2,8 @@ package app.kingmojang.domain.follow.repository
 
 import app.kingmojang.domain.follow.domain.Follow
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
+import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
@@ -10,5 +12,8 @@ interface FollowRepository : JpaRepository<Follow, Long> {
     fun findByCreatorIdAndFollowerId(creatorId: Long, followerId: Long): Follow?
 
     @EntityGraph(attributePaths = ["creator"])
-    fun findAllByFollowerId(followerId: Long, page: Pageable): List<Follow>
+    fun findAllByFollowerId(followerId: Long, page: Pageable): Slice<Follow>
+
+    @EntityGraph(attributePaths = ["creator"])
+    fun findAllByFollowerId(followerId: Long, sort: Sort): List<Follow>
 }

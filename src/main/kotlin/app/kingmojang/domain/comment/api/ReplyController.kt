f4f68b4a315026860ffd5115ee77349c4cdc7d73
SeoSeunghun <proto_seo@naver.com>
2023-06-21 23:30:46 +0900

    feat: update reply api

--- src/main/kotlin/app/kingmojang/domain/comment/api/ReplyController.kt
+++ src/main/kotlin/app/kingmojang/domain/comment/api/ReplyController.kt
@@ -2,8 +2,10 @@ package app.kingmojang.domain.comment.api
 
 import app.kingmojang.domain.comment.application.ReplyService
 import app.kingmojang.domain.comment.dto.request.ReplyRequest
+import app.kingmojang.domain.comment.dto.response.RepliesResponse
 import app.kingmojang.domain.member.domain.UserPrincipal
 import app.kingmojang.global.common.response.CommonResponse
+import org.springframework.data.domain.PageRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -23,7 +25,8 @@ class ReplyController(
         @PathVariable commentId: Long,
         @RequestBody request: ReplyRequest,
     ): ResponseEntity<CommonResponse<Long>> {
-        val replyId = replyService.createReply(userPrincipal, memoId, commentId, request)
+        val memberId = userPrincipal.getId()
+        val replyId = replyService.createReply(memoId, commentId, memberId, request)
 
         val uri = ServletUriComponentsBuilder
             .fromCurrentContextPath().path("/memos/${memoId}/comments/${commentId}/replies/${replyId}")
@@ -39,7 +42,8 @@ class ReplyController(
         @PathVariable replyId: Long,
         @RequestBody request: ReplyRequest,
     ): ResponseEntity<CommonResponse<Long>> {
-        return ResponseEntity.ok().body(CommonResponse.success(replyService.updateReply(userPrincipal, replyId, request)))
+        val memberId = userPrincipal.getId()
+        return ResponseEntity.ok().body(CommonResponse.success(replyService.updateReply(replyId, memberId, request)))
     }
 
     @DeleteMapping("/replies/{replyId}")
@@ -48,7 +52,31 @@ class ReplyController(
         @AuthenticationPrincipal userPrincipal: UserPrincipal,
         @PathVariable replyId: Long,
     ): ResponseEntity<CommonResponse<Void>> {
-        replyService.deleteReply(userPrincipal, replyId)
+        val memberId = userPrincipal.getId()
+        replyService.deleteReply(replyId, memberId)
         return ResponseEntity.ok().body(CommonResponse.success())
+    }
+
+    @GetMapping("/comments/{commentId}/replies")
+    fun readReplies(
+        @AuthenticationPrincipal userPrincipal: UserPrincipal?,
+        @PathVariable commentId: Long,
+        @RequestParam(defaultValue = "0") page: Int,
+        @RequestParam(defaultValue = "10") size: Int,
+    ): ResponseEntity<CommonResponse<RepliesResponse>> {
+        val memberId = userPrincipal?.getId()
+        val request = PageRequest.of(page, size)
+        val replies = replyService.readReplies(commentId, memberId, request)
+        return ResponseEntity.ok().body(CommonResponse.success(replies))
+    }
+
+    @GetMapping("/members/{memberId}/replies")
+    fun readRepliesByMember(
+        @PathVariable memberId: Long,
+        @RequestParam(defaultValue = "0") page: Int,
+        @RequestParam(defaultValue = "20") size: Int,
+    ) : ResponseEntity<CommonResponse<RepliesResponse>> {
+        val request = PageRequest.of(page, size)
+        return ResponseEntity.ok().body(CommonResponse.success(replyService.readRepliesByMember(memberId, request)))
     }
 }

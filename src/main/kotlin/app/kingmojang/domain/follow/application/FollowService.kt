6a32eb432b74b0aec3de4d4f53664653091f7e86
SeoSeunghun <proto_seo@naver.com>
2023-06-21 23:44:08 +0900

    feat: add read follow list logic

--- src/main/kotlin/app/kingmojang/domain/follow/application/FollowService.kt
+++ src/main/kotlin/app/kingmojang/domain/follow/application/FollowService.kt
@@ -2,6 +2,7 @@ package app.kingmojang.domain.follow.application
 
 import app.kingmojang.domain.follow.domain.Follow
 import app.kingmojang.domain.follow.dto.response.FollowResponse
+import app.kingmojang.domain.follow.dto.response.FollowsResponse
 import app.kingmojang.domain.follow.exception.FollowAlreadyExistException
 import app.kingmojang.domain.follow.exception.NotFoundFollowException
 import app.kingmojang.domain.follow.repository.FollowRepository
@@ -9,7 +10,6 @@ import app.kingmojang.domain.member.domain.UserPrincipal
 import app.kingmojang.domain.member.exception.NotFoundMemberException
 import app.kingmojang.domain.member.repository.MemberRepository
 import app.kingmojang.global.common.request.CommonPageRequest
-import app.kingmojang.global.validator.MemberIdValidator
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.data.repository.findByIdOrNull
@@ -17,14 +17,13 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class FollowService(
     private val memberRepository: MemberRepository,
     private val followRepository: FollowRepository,
 ) {
 
-    @Transactional
-    fun createFollow(userPrincipal: UserPrincipal, followerId: Long, creatorId: Long): Long {
-        MemberIdValidator.validate(userPrincipal.getId(), followerId)
+    fun createFollow(followerId: Long, creatorId: Long): Long {
         if (followRepository.existsByCreatorIdAndFollowerId(creatorId, followerId)) {
             throw FollowAlreadyExistException(followerId, creatorId)
         }
@@ -33,9 +32,7 @@ class FollowService(
         return followRepository.save(Follow.create(creator, follower)).id!!
     }
 
-    @Transactional
-    fun deleteFollow(userPrincipal: UserPrincipal, followerId: Long, creatorId: Long) {
-        MemberIdValidator.validate(userPrincipal.getId(), followerId)
+    fun deleteFollow(followerId: Long, creatorId: Long) {
         val follow = (followRepository.findByCreatorIdAndFollowerId(creatorId, followerId)
             ?: throw NotFoundFollowException(followerId, creatorId))
         follow.remove()
@@ -43,10 +40,8 @@ class FollowService(
     }
 
     @Transactional(readOnly = true)
-    fun readFollowByFollower(memberId: Long, request: CommonPageRequest): List<FollowResponse> {
-        return followRepository.findAllByFollowerId(
-            memberId,
-            PageRequest.of(request.page.toInt(), request.size.toInt(), Sort.by("createdAt").descending())
-        ).map { FollowResponse.of(it) }
+    fun readFollowByFollower(memberId: Long, request: PageRequest): FollowsResponse {
+        val follows = followRepository.findAllByFollowerId(memberId, request.withSort(Sort.by("createdAt").descending()))
+        return FollowsResponse.of(follows)
     }
 }

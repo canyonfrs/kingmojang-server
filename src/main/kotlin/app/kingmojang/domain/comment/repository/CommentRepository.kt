a56a80009458908dace8c8ea176982c40a984acd
SeoSeunghun <proto_seo@naver.com>
2023-06-21 23:41:07 +0900

    feat: update comment, reply repository

--- src/main/kotlin/app/kingmojang/domain/comment/repository/CommentRepository.kt
+++ src/main/kotlin/app/kingmojang/domain/comment/repository/CommentRepository.kt
@@ -1,7 +1,15 @@
 package app.kingmojang.domain.comment.repository
 
 import app.kingmojang.domain.comment.domain.Comment
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CommentRepository : JpaRepository<Comment, Long> {
+    @EntityGraph(attributePaths = ["member"])
+    fun findAllWithWriterByMemoIdAndDeletedFalse(memoId: Long, pageable: Pageable): Page<Comment>
+
+    @EntityGraph(attributePaths = ["member"])
+    fun findAllWithWriterByWriterIdAndDeletedFalse(memberId: Long, pageable: Pageable): Page<Comment>
 }

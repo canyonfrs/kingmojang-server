3eb2e9d47760898f2fa093a68d6854e06a824d73
SeoSeunghun <proto_seo@naver.com>
2023-06-21 23:48:19 +0900

    feat: update memo, comment, reply like repository

--- src/main/kotlin/app/kingmojang/domain/like/repository/ReplyLikeRepository.kt
+++ src/main/kotlin/app/kingmojang/domain/like/repository/ReplyLikeRepository.kt
@@ -1,10 +1,15 @@
 package app.kingmojang.domain.like.repository
 
 import app.kingmojang.domain.like.domain.ReplyLike
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ReplyLikeRepository : JpaRepository<ReplyLike, Long> {
+    @EntityGraph(attributePaths = ["reply"])
     fun findByReplyIdAndMemberId(replyId: Long, memberId: Long): ReplyLike?
 
     fun findAllByReplyId(replyId: Long): List<Long>
+
+    @EntityGraph(attributePaths = ["member"])
+    fun findAllByReplyIdInAndMemberId(replyIds: List<Long>, memberId: Long): List<ReplyLike>
 }

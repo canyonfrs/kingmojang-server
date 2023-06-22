ec426c4a6858878a5139a5573c2f22785ff62210
SeoSeunghun <proto_seo@naver.com>
2023-06-22 11:10:35 +0900

    feat: update fixture

--- src/test/kotlin/app/kingmojang/fixture/ReplyFixture.kt
+++ src/test/kotlin/app/kingmojang/fixture/ReplyFixture.kt
@@ -13,14 +13,18 @@ const val REPLY_ID: Long = 1L
 const val REPLY_CONTENT: String = "답글입니다."
 const val REPLY_WRITER: String = "답글 작성자"
 const val REPLY_LIKE_COUNT = 0
+const val REPLY_DELETED = false
 const val REPLY_IS_LIKE = false
 const val REPLY_LIKE_ID = 1L
 val REPLY_CREATED_AT: LocalDateTime = LocalDateTime.of(2023, 5, 1, 12, 30, 41, 311)
 val REPLY_LIKE_CREATED_AT: LocalDateTime = LocalDateTime.of(2023, 5, 1, 12, 30, 41, 311)
 val REPLY_UPDATED_AT: LocalDateTime = LocalDateTime.of(2023, 5, 1, 12, 42, 52, 128)
 
-fun createReplyRequest(): ReplyRequest {
-    return ReplyRequest(MEMBER_ID, REPLY_CONTENT, EMOJI_ID)
+fun createReplyRequest(
+    content: String = REPLY_CONTENT,
+    emojiId: Long = EMOJI_ID
+): ReplyRequest {
+    return ReplyRequest(content, emojiId)
 }
 
 fun createReplyResponse(
@@ -28,11 +32,12 @@ fun createReplyResponse(
     writer: String = REPLY_WRITER,
     content: String = REPLY_CONTENT,
     likeCount: Int = REPLY_LIKE_COUNT,
+    deleted: Boolean = REPLY_DELETED,
     isLike: Boolean = REPLY_IS_LIKE,
     createdAt: LocalDateTime = REPLY_CREATED_AT,
     updatedAt: LocalDateTime = REPLY_UPDATED_AT
 ): ReplyResponse {
-    return ReplyResponse(replyId, writer, content, likeCount, isLike, createdAt, updatedAt)
+    return ReplyResponse(replyId, writer, content, likeCount, deleted, isLike, createdAt, updatedAt)
 }
 
 fun createReply(

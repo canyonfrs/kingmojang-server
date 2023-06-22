b46da93b1b777274e41ac86f7d33128fb735e236
SeoSeunghun <proto_seo@naver.com>
2023-06-22 13:19:23 +0900

    feat: add Invalid Member Type, Invalid Member Autority

--- src/main/kotlin/app/kingmojang/global/exception/ErrorCodes.kt
+++ src/main/kotlin/app/kingmojang/global/exception/ErrorCodes.kt
@@ -21,6 +21,8 @@ enum class ErrorCodes(val status: HttpStatus, val description: String) {
     BAD_CREDENTIAL_EXCEPTION(HttpStatus.UNAUTHORIZED, ""),
     INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
     INVALID_JWT_SIGNATURE(HttpStatus.UNAUTHORIZED, "토큰의 시그니처가 잘못되었습니다."),
+    INVALID_MEMBER_TYPE(HttpStatus.UNAUTHORIZED, "해당 회원의 타입이 잘못되었습니다."),
+    INVALID_MEMBER_AUTHORITY(HttpStatus.UNAUTHORIZED, "해당 회원의 권한이 부족합니다."),
     UNSUPPORTED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "지원하지 않는 JWT 토큰 입니다."),
     EXPIRED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "Access 토큰이 만료되었습니다."),
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "Refresh 토큰이 만료되었습니다."),

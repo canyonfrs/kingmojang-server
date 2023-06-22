06086bb552f019bca359b1a14dd839812860a5ff
SeoSeunghun <proto_seo@naver.com>
2023-06-22 11:10:19 +0900

    feat: modify to add member id

--- src/test/kotlin/app/kingmojang/annotation/WithMockCustomUser.kt
+++ src/test/kotlin/app/kingmojang/annotation/WithMockCustomUser.kt
@@ -1,10 +1,12 @@
 package app.kingmojang.annotation
 
 import app.kingmojang.domain.member.domain.MemberType
+import app.kingmojang.fixture.MEMBER_ID
 import org.springframework.security.test.context.support.WithSecurityContext
 
 @Retention(AnnotationRetention.RUNTIME)
 @WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory::class)
 annotation class WithMockCustomUser(
+    val memberId: Long = MEMBER_ID,
     val type: MemberType = MemberType.CREATOR
 )

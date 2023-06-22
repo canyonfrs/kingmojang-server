01088e3bb7e90a3a6a3435ed93d4d560d4a54d43
SeoSeunghun <proto_seo@naver.com>
2023-06-22 11:09:00 +0900

    feat: update memo repository

--- src/main/kotlin/app/kingmojang/domain/memo/repository/MemoRepository.kt
+++ src/main/kotlin/app/kingmojang/domain/memo/repository/MemoRepository.kt
@@ -1,8 +1,23 @@
 package app.kingmojang.domain.memo.repository
 
 import app.kingmojang.domain.memo.domain.Memo
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
+import org.springframework.data.domain.Sort
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MemoRepository : JpaRepository<Memo, Long> {
 
+    @EntityGraph(attributePaths = ["writer"])
+    fun findMemoWithWriterByIdAndDeletedFalse(id: Long): Memo?
+
+    @EntityGraph(attributePaths = ["writer"])
+    fun findAllWithWriterByWriterIdAndDeletedFalse(writerId: Long, pageable: Pageable): Slice<Memo>
+
+    @EntityGraph(attributePaths = ["writer"])
+    fun findAllWithWriterByWriterIdInAndDeletedFalse(writerIds: List<Long>, pageable: Pageable): Slice<Memo>
+
+    @EntityGraph(attributePaths = ["writer"])
+    fun findAllWithWriterByDeletedFalse(pageable: Pageable): Slice<Memo>
 }

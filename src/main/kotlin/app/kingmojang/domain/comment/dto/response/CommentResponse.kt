ea1b9e98856d7b424832fc52e31f297bc171cd7d
SeoSeunghun <proto_seo@naver.com>
2023-06-23 17:16:05 +0900

    feat: add highlight request, response dto

--- src/main/kotlin/app/kingmojang/domain/comment/dto/response/CommentResponse.kt
+++ src/main/kotlin/app/kingmojang/domain/comment/dto/response/CommentResponse.kt
@@ -1,12 +1,14 @@
 package app.kingmojang.domain.comment.dto.response
 
 import app.kingmojang.domain.comment.domain.Comment
+import app.kingmojang.domain.highlight.dto.response.HighlightResponse
 import java.time.LocalDateTime
 
 data class CommentResponse(
     val commentId: Long,
     val writer: String,
     val content: String,
+    val highlight: HighlightResponse?,
     val likeCount: Int,
     val replyCount: Int,
     val deleted: Boolean,
@@ -20,6 +22,7 @@ data class CommentResponse(
                 commentId = comment.id!!,
                 writer = comment.writer.nickname,
                 content = comment.content,
+                highlight = comment.highlight?.let { HighlightResponse.of(it) },
                 likeCount = comment.likeCount,
                 replyCount = comment.replyCount,
                 deleted = comment.deleted,

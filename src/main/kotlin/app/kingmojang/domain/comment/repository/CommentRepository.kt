9662f150b468251e8d4ff0d6b84265555bdbe34e
ProtoSeo <proto_seo@naver.com>
2023-07-08 16:53:19 +0900

    fix: add findComment method and fix entity graph path
    - add find Comment method with deleted status false
    - update entity graph path member to writer

--- src/main/kotlin/app/kingmojang/domain/comment/repository/CommentRepository.kt
+++ src/main/kotlin/app/kingmojang/domain/comment/repository/CommentRepository.kt
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CommentRepository : JpaRepository<Comment, Long> {
-    @EntityGraph(attributePaths = ["member", "highlight"])
-    fun findAllWithWriterByMemoIdAndDeletedFalse(memoId: Long, pageable: Pageable): Page<Comment>
+    @EntityGraph(attributePaths = ["writer"])
+    fun findCommentWithWriterByIdAndDeletedFalse(commentId: Long): Comment?
 
-    @EntityGraph(attributePaths = ["member", "highlight"])
+    @EntityGraph(attributePaths = ["writer", "highlight"])
+    fun findAllWithWriterByMemoIdAndMemoDeletedFalseAndDeletedFalse(memoId: Long, pageable: Pageable): Page<Comment>
+
+    @EntityGraph(attributePaths = ["writer", "highlight"])
     fun findAllWithWriterByWriterIdAndDeletedFalse(memberId: Long, pageable: Pageable): Page<Comment>
 }
